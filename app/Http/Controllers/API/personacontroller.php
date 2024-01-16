21e7198ff6fd4f959ea6a0ec1b698209b3dc1499
CirJde04 <CirJde04@comfamiliar.com>
2024-01-16 13:17:24 -0500

    Commit controlador persona

--- app/Http/Controllers/API/personacontroller.php
+++ app/Http/Controllers/API/personacontroller.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use APP\Models\Persona;
+use PhpParser\Node\Expr\Yield_;
+use PhpParser\Node\Stmt\TryCatch;
+use Route;
 
 class personacontroller extends Controller
 {
-    //
-
+    // Listar todo de la api
     //Get Listar
     public function Get(){
         try { 
@@ -20,6 +22,35 @@ class personacontroller extends Controller
         }
     }
 
+    //Listar por un registro
+    public function getById($id){
+        try {
+            $data = Persona::find($id);
+            return response()->json($data,200);
+        } catch (\Throwable $th) {
+            return response()->json(["error"=>$th->getMessage()],500);
+        }
+    }
 
     //create //crear persona y todos los eventos de la API
+    public function create(Request $request)
+    {
+        try {
+            $data['nombre'] = $request['nombre'];
+            $data['direccion'] = $request['direccion'];
+            $data['telefono'] = $request['telefono'];
+
+            $res = Persona::create($data);
+            return response()->json($data,200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+
+
+    //Route::get('/',[ personacontroller::class, 'get']);
+    //Route::get('/',[ personacontroller::class, 'create']);
+
 }
